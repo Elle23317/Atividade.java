b9382684ec184f5995ede27dfdac0d4a1b8bdf9f
Hiraeth233177 <145845132+Elle23317@users.noreply.github.com>
2025-05-27 18:58:59 -0300

    Atualizar o Porta.java

--- Porta.java
+++ Porta.java
@@ -52,7 +52,7 @@ public class Porta{
 
 public class PrincipalPorta{
     public static void main(String arg[]){
-        Porta p = new Porta("rosa", 9, 2, false);
+        Porta p = new Porta("roxo", 5, 7, false);
         System.out.println(p);
         p.setEstaAberta(true);
         System.out.println(p);
