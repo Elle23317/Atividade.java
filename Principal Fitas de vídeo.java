b62176e1f0bf2ef5c99cc9eb086fd064136fc121
Hiraeth233177 <145845132+Elle23317@users.noreply.github.com>
2025-08-05 16:22:02 -0300

    Principal Fitas de vídeo.java

---
+++ Principal Fitas de vídeo.java
@@ -0,0 +1,6 @@
+public class PrincipalFitasdevideo {
+    public static void main(String[] args){
+        Fitasdevideo f = new Fitasdevideo("Lanternas vermelhas", 15);
+        f.imprimir(3);
+    }
+}
